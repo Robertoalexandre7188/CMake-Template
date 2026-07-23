df5ffcbf09e28a23b0a81abe0bbb6db73486183a
Matheus Castro de Inchoste <minchoste@gmail.com>
2026-07-22 23:55:14 -0300

    fix: CMakeDependencies was very faulty

--- cmake/toolchain/x86_64-windows-gnu/LLVM.cmake
+++ cmake/toolchain/x86_64-windows-gnu/LLVM.cmake
@@ -1,13 +1,16 @@
-include(Default)
+include(${CMAKE_CURRENT_LIST_DIR}/Default.cmake)
 
-find_program(CMAKE_C_COMPILER NAMES clang REQUIRED)
-find_program(CMAKE_LINKER NAMES lld REQUIRED)
-find_program(CMAKE_AR NAMES llvm-ar REQUIRED)
-find_program(CMAKE_RANLIB NAMES llvm-ranlib REQUIRED)
-find_program(CMAKE_NM NAMES llvm-nm REQUIRED)
+if(NOT CMAKE_HOST_WIN32 OR NOT CMAKE_SYSTEM_PROCESSOR MATCHES "(x86_64|AMD64)")
+    find_program(CMAKE_C_COMPILER NAMES clang REQUIRED)
+    find_program(CMAKE_CXX_COMPILER NAMES clang++ REQUIRED)
+    find_program(CMAKE_LINKER NAMES lld REQUIRED)
+    find_program(CMAKE_AR NAMES llvm-ar REQUIRED)
+    find_program(CMAKE_RANLIB NAMES llvm-ranlib REQUIRED)
+    find_program(CMAKE_NM NAMES llvm-nm REQUIRED)
 
-set(CMAKE_C_COMPILER_TARGET "${PLATFORM}")
-set(CMAKE_LINKER_TYPE LLD)
+    set(CMAKE_C_COMPILER_TARGET "${PLATFORM}")
+    set(CMAKE_LINKER_TYPE LLD)
+endif()
 
 execute_process(
     COMMAND ${CMAKE_C_COMPILER} -target ${PLATFORM} -print-search-dirs
